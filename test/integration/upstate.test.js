db9837535bd348d2e8c4894adad59b70c6e4fca8
Kristoffer Walker <kris@kixx.name>
2015-04-18 15:47:50 -0400

    Tests for process run.
    	modified:   test/integration/upstate.test.js

--- test/integration/upstate.test.js
+++ test/integration/upstate.test.js
@@ -28,4 +28,72 @@ describe("integration upstate", function () {
 
   });
 
+  describe('with help option', function () {
+    var
+    RES   = null,
+    LINES = null;
+
+    before(function (done) {
+      TLIB.exec(['--help'])
+        .then(function (res) {
+          RES = res;
+          LINES = RES.stdout.trim().split('\n');
+        })
+        .then(done, done);
+    });
+
+    it("has a usage line", function () {
+      LINES[1].should.equal('ups <command> <task> [options]');
+    });
+
+    it("has a command line", function () {
+      LINES[3].should.match(/commands/i);
+    });
+
+    it("has a options line", function () {
+      LINES[8].should.match(/options/i);
+    });
+
+  });
+
+  describe('tasks command', function () {
+    var
+    RES   = null,
+    LINES = null;
+
+    before(function (done) {
+      TLIB.exec(['tasks'])
+        .then(function (res) {
+          RES = res;
+          LINES = RES.stdout.trim().split('\n');
+        })
+        .then(done, done);
+    });
+
+    it("has a tasks line", function () {
+      LINES[1].should.match(/tasks/i);
+    });
+
+  });
+
+  describe('run command with no task', function () {
+    var
+    RES   = null,
+    LINES = null;
+
+    before(function (done) {
+      TLIB.exec(['run'])
+        .then(function (res) {
+          RES = res;
+          LINES = RES.stdout.trim().split('\n');
+        })
+        .then(done, done);
+    });
+
+    xit("has a tasks line", function () {
+      LINES[1].should.match(/tasks/i);
+    });
+
+  });
+
 });
